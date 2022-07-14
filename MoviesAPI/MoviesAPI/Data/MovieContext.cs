9a176f402210475ecc47c037ea8c24c981e6012b
rodvm <108483205+rodvm@users.noreply.github.com>
2022-07-14 16:42:11 -0600

    add entity packages

---
+++ MoviesAPI/MoviesAPI/Data/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
+namespace ContosoPizza.Data;
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movies => Set<Movie>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<MovieImage> MovieImages => Set<MovieImage>();
+    public DbSet<MovieApproach> MovieApproaches => Set<MovieApproach>();
+}
