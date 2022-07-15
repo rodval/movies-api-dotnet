2f1f7aa0af7aded31ec53541ab12659d9dbe06c9
rodvm <108483205+rodvm@users.noreply.github.com>
2022-07-15 14:31:20 -0600

    add jsonignore propertie

--- MoviesAPI/MoviesAPI/Models/MovieImage.cs
+++ MoviesAPI/MoviesAPI/Models/MovieImage.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+
 namespace MoviesAPI.Models
 {
     public class MovieImage
     {
         public int Id { get; set; }
         public string? UrlImage { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Movie>? Movies { get; set; }
     }
 }
 
--- MoviesAPI/MoviesAPI/Models/User.cs
+++ MoviesAPI/MoviesAPI/Models/User.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using MoviesAPI.Utilities;
+using System.Text.Json.Serialization;
 
 namespace MoviesAPI.Models
 {
     public class User
     {
         public int Id { get; set; }
+        
         public string? Name { get; set; }
+        
         public string? UserName { get; set; }
+
         public UserRoleType Role { get; set; }
+
+        [JsonIgnore]
         public ICollection<Movie>? LikedMovies { get; set; }
     }
 
