49a17ed8868d18c00e582795b77db09f3e5b9b95
rodvm <108483205+rodvm@users.noreply.github.com>
2022-07-15 08:41:07 -0600

    create user service layer

---
+++ MoviesAPI/MoviesAPI/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MoviesAPI.Services;
+using MoviesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        UserService _service;
+
+        public UserController(UserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPut("{id}/addlikedmovie")]
+        public IActionResult AddTopping(int id, int movieId)
+        {
+            var validUser = _service.GetById(id);
+
+            if (validUser is not null)
+            {
+                _service.AddLikedMovie(id, movieId);
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
+
---
+++ MoviesAPI/MoviesAPI/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Services
+{
+    public class UserService
+    {
+        public UserService()
+        {
+        }
+
+        public User? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLikedMovie(int UserId, int MovieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
