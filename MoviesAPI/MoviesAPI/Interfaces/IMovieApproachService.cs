d7a3a2bbd2a52cf6654cae608005fe39a81a256d
rodvm <108483205+rodvm@users.noreply.github.com>
2022-07-17 09:38:12 -0600

    add movie approach interface

--- MoviesAPI/MoviesAPI/Interfaces/IMovieApproachService.cs
+++ MoviesAPI/MoviesAPI/Interfaces/IMovieApproachService.cs
@@ -5,6 +5,10 @@ namespace MoviesAPI.Interfaces
 {
     public interface IMovieApproachService
     {
+        public MovieApproach? GetById(int id);
+        public IEnumerable<MovieApproach> GetAll(int id);
+        public MovieApproach? Create(MovieApproach newMovie);
+        public void Update(int id, MovieApproach updateMovie);
     }
 }
 
