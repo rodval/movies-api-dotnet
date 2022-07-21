ac3bbf458d5f82b82a602420af98946364915580
rodvm <108483205+rodvm@users.noreply.github.com>
2022-07-21 16:45:57 -0600

    set up unittest proyect

---
+++ MoviesAPI/MovieAPI.UnitTest/MoviesValidatorShouldGet.cs
@@ -0,0 +1,18 @@
+﻿
+using MoviesAPI.Models;
+
+namespace MovieAPI.UnitTest
+{
+    public class MoviesValidatorShouldGet
+    {
+        [Fact]
+        public void GetMovies_ReturnsNotEmpty()
+        {
+            string path = @"../../../Utilities/Movies.Json";
+            var streamReaderJSON = new StreamReaderJSON<Movie>();
+            var data = streamReaderJSON.Get(path);
+
+            Assert.NotEmpty(data);
+        }
+    }
+}
---
+++ MoviesAPI/MovieAPI.UnitTest/StreamReaderJSON.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MovieAPI.UnitTest
+{
+    public class StreamReaderJSON<T>
+    {
+        public List<T> Get(string path)
+        {
+            try
+            {
+                using StreamReader jsonStream = File.OpenText(path);
+                var json = jsonStream.ReadToEnd();
+                var data = JsonConvert.DeserializeObject<List<T>>(json);
+                return data.Count > 0 ? data : new List<T>();
+            }
+            catch
+            {
+                throw new Exception("could not find or read the requested resource.");
+            }
+        }
+    }
+}
+
