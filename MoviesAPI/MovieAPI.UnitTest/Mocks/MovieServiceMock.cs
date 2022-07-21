2c20a79d69d1b1a3b8afd8694c1d1712ca3ab6d7
rodvm <108483205+rodvm@users.noreply.github.com>
2022-07-21 17:01:48 -0600

    create movieServiceMock

---
+++ MoviesAPI/MovieAPI.UnitTest/Mocks/MovieServiceMock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MoviesAPI.Interfaces;
+using MoviesAPI.Models;
+
+namespace MovieAPI.UnitTest.Mocks
+{
+    public class MovieServiceMock : IMovieService
+    {
+        public void AddMovieImage(int userId, int MovieId, int MovieImageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie? Create(int userId, Movie newMovie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int userId, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetAllMovies(int userId, int numberOfResults, bool availability)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int userId, Movie updateMovie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
